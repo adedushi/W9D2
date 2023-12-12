edddc513c809e9d54d2bb443ec52125b0340212b
Albert Dedushi <adedushi@hotmail.com>
2023-12-12 15:12:34 -0500

    complete setupBoard

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,10 @@
-Add your import statements for View and Game here
+import View from "./ttt-view.js"
+import Game from "../ttt_node/game.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const ttt = document.querySelector("figure")
+  const game = new Game()
+  const a = new View(game, ttt)
+  a.setupBoard()
 });
+
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,11 +1,24 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
   }
   
   setupBoard() {
+    const ul = document.createElement("ul");
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){ 
+      let liElement = document.createElement("li")
+      liElement.dataset.position = [i,j]
+      ul.appendChild(liElement)
+    }
+  }
+  
+    this.el.appendChild(ul)
   }
   
   handleClick(e) {
+
   }
 
   makeMove(square) {
